2fa460805239a89eed91b5edec767b25284648e5
tokar_ilya <itokar98@mail.ru>
2020-04-17 20:17:40 +0300

    added user token check

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
+using TodoApi.Models.JsonModel;
 
 namespace TodoApi.Controllers
 {
@@ -15,64 +16,99 @@ namespace TodoApi.Controllers
     [ApiController]
     public class TodoItemsController : Controller
     {
-        private readonly TodoContext _context;
+        private readonly TodoContext _dbcontext;
         private readonly IHttpClientFactory _clientFactory;
 
-        public TodoItemsController(TodoContext context, IHttpClientFactory clientFactory)
+        private Getlogin _userinfo;
+
+        public TodoItemsController(TodoContext dbcontext, IHttpClientFactory clientFactory)
         {
-            _context = context;
+            _dbcontext = dbcontext;
             _clientFactory = clientFactory;
         }
 
         // GET: api/TodoItems (Get All Items)
         [HttpGet]
-        public IActionResult GetTodoItem()
+        public async Task<ActionResult<TodoItem>> GetTodoItem()
         {
-            List<TodoItem> items = _context.TodoItems.ToList();
-            return Ok(items);
+            Getlogin userinfo = await GetloginUser();
+
+            var todoItems = _dbcontext.TodoItems
+                .Where(b => b.UserId == userinfo.id).ToList();
+            return Ok(todoItems);
         }
 
         // GET: api/TodoItems/5 (Get item)
         [HttpGet("{id}")]
         public async Task<ActionResult<TodoItem>> GetTodoItem(int id)
         {
-            var todoItem = await _context.TodoItems.FindAsync(id);
+            Getlogin userinfo = await GetloginUser();
+
+            var todoItem = await _dbcontext.TodoItems.FindAsync(id);
 
             if (todoItem == null)
             {
                 return NotFound();
             }
 
-            return todoItem;
+            if(userinfo.id == todoItem.UserId)
+            {
+                return todoItem;
+            }
+
+            Response.StatusCode = 403;
+            await Response.WriteAsync("Ошибка доступа");
+            return null;
         }
 
         // DELETE: api/TodoItems/5 (Remove Item)
         [HttpDelete("{id}")]
         public async Task<ActionResult<TodoItem>> DeleteTodoItem(int id)
         {
-            var todoItem = await _context.TodoItems.FindAsync(id);
+            Getlogin userinfo = await GetloginUser();
+
+            if(userinfo == null)
+            {
+                Response.StatusCode = 401;
+                await Response.WriteAsync("Пользователь не авторизован");
+            }
+
+            var todoItem = await _dbcontext.TodoItems.FindAsync(id);
             if (todoItem == null)
             {
                 return NotFound();
             }
 
-            _context.TodoItems.Remove(todoItem);
-            await _context.SaveChangesAsync();
-
-            return todoItem;
+            if(userinfo.id == todoItem.UserId)
+            {
+                _dbcontext.TodoItems.Remove(todoItem);
+                await _dbcontext.SaveChangesAsync();
+                return todoItem;
+            }
+            
+            Response.StatusCode = 403;
+            await Response.WriteAsync("Ошибка доступа");
+            return null;
         }
 
         // POST: api/TodoItems  (Added Item)
         [HttpPost]
-        public ActionResult<String> CreateTodoItem([FromBody]TodoItemAdd todoItemAdd)
+        public async Task<ActionResult<String>> CreateTodoItem([FromBody]TodoItemAdd todoItemAdd)
         {
+            Getlogin userinfo = await GetloginUser();
+
+            if(userinfo == null){
+                Response.StatusCode = 401;
+                await Response.WriteAsync("Пользователь не авторизован");
+            }
+
             if(todoItemAdd.Title == null || todoItemAdd.Body == null)
             {
-                return BadRequest(new { message = "Title or body is incorrect" });;
+                return BadRequest(new { message = "Title or body is incorrect" });
             }
 
             // Получился очень странный запрос нужно с ним поработать))
-            var todoItems = _context.TodoItems
+            var todoItems = _dbcontext.TodoItems
                 .Where(b => b.OnCreate >= DateTime.Now && 
                     b.OnCreate <=DateTime.Now.AddDays(1) && 
                     b.Title.Contains(todoItemAdd.Title.ToString())
@@ -86,39 +122,37 @@ namespace TodoApi.Controllers
             {
                 Title = todoItemAdd.Title,
                 Body = todoItemAdd.Body,
-                UserId = 12
+                UserId = userinfo.id
             };
 
-            _context.Add(item);
-            _context.SaveChanges();
+            _dbcontext.Add(item);
+            _dbcontext.SaveChanges();
 
             return Ok("Create item");
         }
 
 
-        [HttpGet("testprint")]
-        public IActionResult PrintResult([FromBody]TodoItemAdd todoItemAdd)
+        public async Task<Getlogin> GetloginUser()
         {
-            if(todoItemAdd.Title == null || todoItemAdd.Body == null)
-            {
-                return BadRequest(new { message = "Username or password is incorrect" });;
-            }
-            return Ok(todoItemAdd);
-        }
-
-        // GET: api/TodoItems
-        [HttpGet("testhttp")]
-        public async Task<ActionResult<String>> OnGet()
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get,
-                "https://jsonplaceholder.typicode.com/todos/1");
-            request.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+            var Authorization = Request.Headers["Authorization"].ToString();
+            var request = new HttpRequestMessage(HttpMethod.Post,
+                "http://localhost:4000/api/auth/getlogin");
+            request.Headers.Add("Authorization", Authorization);
 
             var client = _clientFactory.CreateClient();
             var response = await client.SendAsync(request);
             var data = await response.Content.ReadAsStringAsync();
-
-            return data;
+            
+            if(response.StatusCode.ToString() == "OK"){
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    WriteIndented = true
+                };
+                Getlogin userinfo = JsonSerializer.Deserialize<TodoApi.Models.JsonModel.Getlogin>(data, options);
+                return userinfo;
+            }
+            return null;   
         }
 
     }
--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -4,7 +4,7 @@ public class TodoItem
     public int id { get; private set; }
     public string Title { get; set; }
     public string Body { get; set; }
-    public int UserId {get; set;}
+    public long UserId {get; set;}
     public DateTime OnCreate{get; private set;} 
     public TodoItem(){
         OnCreate = DateTime.Now;
