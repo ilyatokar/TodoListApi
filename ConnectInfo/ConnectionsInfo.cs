eb45116a70135fb168607902c2b98d55be1e63a7
SpotlessRadiance <ya.emeraldgirl@yandex.ru>
2020-04-18 00:01:36 +0300

    Added interactions with second server

---
+++ ConnectInfo/ConnectionsInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.ConnectInfo
+{
+    public class ConnectionsInfo
+    {
+        public static readonly string StatusHost = "http://localhost:44345/";
+        public static readonly string AuthHost = "https://80.93.177.181/";
+        //http://localhost:4000/";
+    }
+}
--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -7,8 +7,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using TodoApi.Models;
 using TodoApi.Models.JsonModel;
+using TodoApi.ConnectInfo;
+using System.Text;
 
 namespace TodoApi.Controllers
 {
@@ -43,7 +46,6 @@ namespace TodoApi.Controllers
         public async Task<ActionResult<TodoItem>> GetTodoItem(int id)
         {
             Getlogin userinfo = await GetloginUser();
-
             var todoItem = await _dbcontext.TodoItems.FindAsync(id);
 
             if (todoItem == null)
@@ -53,12 +55,16 @@ namespace TodoApi.Controllers
 
             if(userinfo.id == todoItem.UserId)
             {
-                return todoItem;
+                var response = new
+                {
+                    title = todoItem.Title,
+                    text = todoItem.Body,
+                    completed = GetItemStatus(id),
+                    created = todoItem.OnCreate
+                };
+                return Ok(response);
             }
-
-            Response.StatusCode = 403;
-            await Response.WriteAsync("Ошибка доступа");
-            return null;
+            return StatusCode(403);
         }
 
         // DELETE: api/TodoItems/5 (Remove Item)
@@ -78,12 +84,17 @@ namespace TodoApi.Controllers
             {
                 return NotFound();
             }
-
             if(userinfo.id == todoItem.UserId)
             {
-                _dbcontext.TodoItems.Remove(todoItem);
-                await _dbcontext.SaveChangesAsync();
-                return todoItem;
+                bool deleted = await DeleteItemStatus(todoItem.id);
+                if (deleted)
+                {
+                    _dbcontext.TodoItems.Remove(todoItem);
+                    await _dbcontext.SaveChangesAsync();
+                    return Ok();
+                }
+                else
+                    return BadRequest(new { message = "Server isn't connected" });
             }
             
             Response.StatusCode = 403;
@@ -93,7 +104,7 @@ namespace TodoApi.Controllers
 
         // POST: api/TodoItems  (Added Item)
         [HttpPost]
-        public async Task<ActionResult<String>> CreateTodoItem([FromBody]TodoItemAdd todoItemAdd)
+        public async Task<ActionResult<String>> CreateTodoItem(TodoItemAdd todoItemAdd, bool completed)
         {
             Getlogin userinfo = await GetloginUser();
 
@@ -117,20 +128,47 @@ namespace TodoApi.Controllers
             if(todoItems.Count != 0){
                 return BadRequest(new { message = "A task with the same Name already exists." });
             }
-
+            long statusId = await CreateItem(completed);
+            if (statusId == -1)
+            {
+                return BadRequest("Status can't be saved");
+            }
             TodoItem item = new TodoItem()
             {
                 Title = todoItemAdd.Title,
                 Body = todoItemAdd.Body,
-                UserId = userinfo.id
+                UserId = userinfo.id,
             };
-
             _dbcontext.Add(item);
             _dbcontext.SaveChanges();
-
-            return Ok("Create item");
+            var response = new { id = item.id };
+            return Ok(response);
         }
 
+        [HttpPut]
+        public async Task<ActionResult<String>> PutTodoItem(TodoItem changedItem, bool completed)
+        {
+            Getlogin userinfo = await GetloginUser();
+
+            if (userinfo == null)
+            {
+                Response.StatusCode = 401;
+                await Response.WriteAsync("Пользователь не авторизован");
+            }
+            TodoItem item = _dbcontext.TodoItems.SingleOrDefault(c => c.id==changedItem.id);
+            if (item!=null && item.UserId == userinfo.id)
+            {
+                bool done = await PutItem(item.id, completed);
+                if (!done)
+                    return StatusCode(500, "Something went wrong");
+                _dbcontext.Update(changedItem);
+                _dbcontext.SaveChanges();
+                var response = new { id = item.id };
+                return Ok(response);
+            }
+            return BadRequest();
+            }
+        
 
         public async Task<Getlogin> GetloginUser()
         {
@@ -149,10 +187,85 @@ namespace TodoApi.Controllers
                     PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                     WriteIndented = true
                 };
-                Getlogin userinfo = JsonSerializer.Deserialize<TodoApi.Models.JsonModel.Getlogin>(data, options);
+                Getlogin userinfo = System.Text.Json.JsonSerializer.Deserialize<TodoApi.Models.JsonModel.Getlogin>(data, options);
                 return userinfo;
             }
             return null;   
+        }
+
+
+        private async Task<bool> DeleteItemStatus(long id)
+        {
+            String uri = ConnectionsInfo.StatusHost + $"api/todostatus/{id}";
+            HttpClient _client = _clientFactory.CreateClient();
+            var requestMessage = new HttpRequestMessage(HttpMethod.Delete, uri);
+            requestMessage.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+            var Authorization = Request.Headers["Authorization"].ToString();
+            requestMessage.Headers.Add("Autthorization", Authorization);
+            var response = await _client.SendAsync(requestMessage);
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        private async Task<bool> PutItem(int task_id, bool completed)
+        {
+            string uri = ConnectionsInfo.StatusHost + $"/api/todostatus/new/{task_id}/{completed}";
+            HttpClient _client = _clientFactory.CreateClient();
+            var requestMessage = new HttpRequestMessage(HttpMethod.Put, uri);
+            requestMessage.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+            var Authorization = Request.Headers["Authorization"].ToString();
+            requestMessage.Headers.Add("Autthorization", Authorization);
+            var response = await _client.SendAsync(requestMessage);
+            var content = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private async Task<long> CreateItem(bool completed)
+        {
+            string uri = ConnectionsInfo.StatusHost + $"/api/todostatus/new/{completed}";
+            HttpClient _client = _clientFactory.CreateClient();
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, uri);
+            requestMessage.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+            var Authorization = Request.Headers["Authorization"].ToString();
+            requestMessage.Headers.Add("Autthorization", Authorization);
+            var response = await _client.SendAsync(requestMessage);
+            var content = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                var res = JsonConvert.DeserializeObject<Dictionary<string, string>>(content);
+                return Convert.ToInt64(res["id"]);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        private async Task<String> GetItemStatus(int id)
+        {
+            string uri = ConnectionsInfo.StatusHost + $"/api/todostatus/{id}";
+            HttpClient _client = _clientFactory.CreateClient();
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+            requestMessage.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+            var response = await _client.SendAsync(requestMessage);
+            var content = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                var res = JsonConvert.DeserializeObject<Dictionary<string, string>>(content);
+                return res["status"];
+            }
+            else
+            {
+                return "";
+            }
         }
 
     }
